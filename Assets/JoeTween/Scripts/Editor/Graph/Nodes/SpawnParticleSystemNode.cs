3d727c9ebb0b6bfec4933af798ff153da6eb6c19
Joe Rickwood <98846669+JoeRickwood@users.noreply.github.com>
2026-07-22 17:18:32 +1200

    Gameobjects Can Be Spawned With JOETWEEN

---
+++ Assets/JoeTween/Scripts/Editor/Graph/Nodes/SpawnParticleSystemNode.cs
@@ -0,0 +1,49 @@
+using System;
+using UnityEngine;
+
+namespace JoeTween
+{
+    [Serializable]
+    class SpawnGameObjectNode : TweenActionNode<UnityEngine.Transform>
+    {
+        public float lifetime;
+        public TweenSpace space;
+        public GameObject gameobjectPrefab;
+        public Vector3 spawnPos;
+
+        protected override void OnDefinePorts(IPortDefinitionContext context)
+        {
+            base.OnDefinePorts(context);
+
+            context.AddInputPort<TweenSpace>("Space").Build();
+            context.AddInputPort<Vector3>("Spawn Position").Build();
+            context.AddInputPort<GameObject>("Spawned GameObject").Build();
+            context.AddInputPort<float>("Lifetime").Build(); // -1 For Infinite Lifetime
+        }
+
+        public override TweenAction GetTweenAction(TweenSequenceData _data)
+        {
+            LoadValues();
+
+            SpawnGameObjectTween tween = new SpawnGameObjectTween
+            (
+                animationCurve, tweenLength, false,
+                null, lifetime, space, spawnPos, gameobjectPrefab
+            );
+
+            TweenSequenceNodeExtensions.LoadMod(ref tween.spawnPos, GetInputPortByName("Spawn Position"));
+
+            return tween;
+        }
+
+        protected override void LoadValues()
+        {
+            base.LoadValues();
+
+            GetInputPortByName("Space").TryGetValue(out space);
+            GetInputPortByName("Spawn Position").TryGetValue(out spawnPos);
+            GetInputPortByName("Spawned GameObject").TryGetValue(out gameobjectPrefab);
+            GetInputPortByName("Lifetime").TryGetValue(out lifetime);
+        }
+    }
+}
---
+++ Assets/JoeTween/Scripts/JoeTween/SpawnGameObjectTween.cs
@@ -0,0 +1,75 @@
+/***********************************************************************
+    Auckland
+    New Zealand
+
+    (c) 2026 Joe Rickwood
+
+    File Name   :   SpawnGameObjectTween.cs
+    Description :   Spwans A GameObject On The Start Of The Tween At A Position In The World
+    Author      :   Joe Rickwood
+**************************************************************************/
+
+
+using System;
+using UnityEngine;
+using UnityEngine.VFX;
+
+namespace JoeTween
+{
+    /// <summary>
+    /// Moved A Target Transform Component Over Two Position Vectors
+    /// </summary>
+    [System.Serializable]
+    public class SpawnGameObjectTween : TweenAction<Transform>
+    {
+        public float lifetime;
+        public TweenSpace space;
+        [SerializeReference] public GameObject gameObjectPrefab;
+        public TweenValue<Vector3> spawnPos;
+
+        public SpawnGameObjectTween(
+            AnimationCurve _animationCurve, float _animationLength, bool _looping,
+            Transform _component, float _lifetime, TweenSpace _space, Vector3 _spawnPos, GameObject _gameObjectPrefab
+        ) : base(_animationCurve, _animationLength,_component, _looping)
+        {
+            lifetime = _lifetime;
+            space = _space;
+            gameObjectPrefab = _gameObjectPrefab;
+            spawnPos.value = _spawnPos;
+        }
+
+        public override void CloneModifiers()
+        {
+            base.CloneModifiers();
+
+            if (spawnPos.valueModifier != null)
+                spawnPos.valueModifier = spawnPos.valueModifier.Clone<Vector3>();
+        }
+
+        internal override void StartAction()
+        {
+            spawnPos.OnActionStart(target);
+
+            GameObject cur = GameObject.Instantiate(gameObjectPrefab, spawnPos.GetValue(0.0f), Quaternion.identity);
+            var visualEffect = cur.GetComponent<VisualEffect>();
+
+            if (lifetime !< 0)
+                GameObject.Destroy(cur, lifetime);
+
+            if (visualEffect != null)
+                visualEffect?.Play();
+
+            base.StartAction();
+        }
+
+        protected override void EndAction()
+        {
+            base.EndAction();
+        }
+
+        public override void Update(float _time)
+        {
+            base.Update(_time);
+        }
+    }
+}
